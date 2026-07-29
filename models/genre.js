98a27efe1ce98cde8f9b4e92fc7b6c62602b4ea8
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-07-29 14:27:29 +0700

    feat(models): define genre model schema

---
+++ models/genre.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define("Genre", {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nama_genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
+  
+    return Genre;
+  };
